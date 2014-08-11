f51645794b159e164e20ae0d889f97810fca5a8f
leeseungho <leeseungho90@gmail.com>
2014-08-11 18:11:56 +0200

    export
    using to_stata or to_csv of pandas, Dataframe

--- til_base_model/export.py
+++ til_base_model/export.py
@@ -8,6 +8,7 @@ from pandas import HDFStore, DataFrame, merge # DataFrame
 import numpy as np
 import pdb
 import time
+from pandas import *
 
 from utils import til_name_to_of
 
@@ -29,9 +30,7 @@ duree_vie = (deces>0) * (deces - naiss)
 duree_vie_freq = duree_vie.value_counts()
 print duree_vie_freq
 
-
-
-pdb.set_trace()
+####################pdb.set_trace()
     
 table = {}
 nom = 'person'
@@ -62,7 +61,7 @@ res_size = len(ids)
 #    sum_values[ind,0] = ind
 list2drop = ['wprm_init','age','idmen','idfoy','quifoy', 'pere','mere','conj','dur_in_couple','dur_out_couple',
              'education_level','productivity']
-list2keep = ['sexe','noi','findet','civilstate','workstate','sali','rsti','choi','xpr','anc','unempdur5','inacdur5','invaldur5']
+list2keep = ['sexe','noi','findet','civilstate','workstate','sali','rsti','choi','xpr','anc','unempdur5','inacdur5','invaldur5','isprivate', 'ispublic', 'isindep']
 #tab = table[ent].drop(list2drop, axis=1)
 tab = table['ind'][list2keep]
 indiv = tab.groupby(['noi'],sort=False)
@@ -73,6 +72,7 @@ moyenne = cumul.div(nb_obs,axis=0)
 workstate = tab.groupby(['noi','workstate'],sort=False).size()
 civilstate = tab.groupby(['noi','civilstate'],sort=False).size()
 
+'''
 passage = table['ind'][['noi','period','idmen','idfoy','quifoy','quimen']]
 ## donnee menage
 tabm = output['men']
@@ -83,9 +83,10 @@ cumul = menag.sum()
 nb_obs = menag.size()
 moyenne = cumul.div(nb_obs,axis=0)
 decile = tabm.groupby(['noi','decile'],sort=False).size()
-
-df = DataFrame(columns=list(list2keep))
-pdb.set_trace()
-df.to_Stata("C:\Simulation\simul.dta")
+'''
+df = DataFrame(tab, columns=list(list2keep))
+#####pdb.set_trace()
+df.to_stata(tab,"C:\Simulation\simul.dta")
+df.to_csv(tab,"C:\Simulation\simul.csv")
 simul.close()
 output.close()
