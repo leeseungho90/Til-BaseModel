504e633ee02994789434b6d3ebdd0d3460962d68
leeseungho <leeseungho90@gmail.com>
2014-09-01 10:26:45 +0200

    last

--- til_base_model/export.py
+++ til_base_model/export.py
@@ -87,7 +87,7 @@ decile = tabm.groupby(['noi','decile'],sort=False).size()
 df = DataFrame(tab, columns=list(list2keep))
 #####pdb.set_trace()
 
-df.to_csv("C:\Simulation\simul20.csv")
+df.to_csv("C:\Users\s.lee\Dropbox\IPP\TIL\simul5.csv")
 #df.to_stata("C:\Simulation\simul.dta")
 '''
 df.to_csv("C:\Simulation\destinie.csv")
